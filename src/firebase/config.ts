6bf8e04a6c70e8f788e9ad7e1f826fba8e9082c9
Steven Penner <pennersteven0@gmail.com>
2025-11-13 19:54:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "almost-done-02144205-1510a",
+  "appId": "1:386247171412:web:006628edd14a61c1583cc1",
+  "apiKey": "AIzaSyBWL6VvM3UuEegBS7aSZ1AjbYxCYe2zjxY",
+  "authDomain": "almost-done-02144205-1510a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "386247171412"
+};
